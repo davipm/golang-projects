d4b5a857b5cde3c6960ab4c86670f84c1a7e98d2
davi <davi.p.m94@gmail.com>
2021-02-02 22:56:57 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func helloWorld() {
+	fmt.Println("Hello World!")
+}
+
+func valuesExample() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+func variablesExample() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
+
+// main application
+func main() {
+	helloWorld()
+	valuesExample()
+	variablesExample()
+}
