91ea02638df6ea04ef13c1d738e543c5da3bd5fb
davi <davi.p.m94@gmail.com>
2021-02-02 23:02:36 -0300

    adding more stuff

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func helloWorld() {
@@ -36,9 +37,49 @@ func variablesExample() {
 	fmt.Println(f)
 }
 
+const s string = "constant"
+
+func constantsExample() {
+	fmt.Println(s)
+
+	const n = 500000000
+	const d = 3e20 / n
+
+	fmt.Println(d)
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
+}
+
+func forExample() {
+	i := 1
+
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
+
 // main application
 func main() {
 	helloWorld()
 	valuesExample()
 	variablesExample()
+	constantsExample()
+	forExample()
 }
