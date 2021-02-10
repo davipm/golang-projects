0dade04050bc52c5d7cf1fd7669d31056d15b0b1
davi <davi.p.m94@gmail.com>
2021-02-09 23:29:03 -0300

    update slice example

--- main.go
+++ main.go
@@ -157,6 +157,38 @@ func sliceExample() {
 	fmt.Println("set", s[2])
 
 	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd", s)
+
+	var c = make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy", c)
+
+	var l = s[2:5]
+	fmt.Println("sl1", l)
+
+	l = s[:5]
+	fmt.Println("sl2", l)
+
+	l = s[2:]
+	fmt.Println("sl3", l)
+
+	var t = []string{"g", "h", "i"}
+	fmt.Println("dcl", t)
+
+	var twoD = make([][]int, 3)
+
+	for i := 0; i < 3; i++ {
+		var innerLen = i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+
+	fmt.Println("2d", twoD)
 }
 
 // main application
