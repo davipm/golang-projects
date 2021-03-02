5141301864194c6e281a3672f7ba1357ab7d2068
davi <davi.p.m94@gmail.com>
2021-03-02 19:54:03 -0300

    adding range example

--- main.go
+++ main.go
@@ -239,6 +239,37 @@ func mapExamples() {
 	fmt.Println("map", n)
 }
 
+func rangeExample() {
+	nums := []int{2, 3, 4}
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	fmt.Println("sum", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index", i)
+		}
+	}
+
+	ksv := map[string]string{"a": "apple", "b": "banana"}
+
+	for k, v := range ksv {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range ksv {
+		fmt.Println("key", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
+
 // main application
 func main() {
 	helloWorld()
@@ -253,4 +284,5 @@ func main() {
 	fmt.Println(distance("david", "deved"))
 	fmt.Println("factorial result ", factorial(5))
 	mapExamples()
+	rangeExample()
 }
