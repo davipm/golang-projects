47887f9c1e27a57f6957373e33d13f22fd17ffbc
davi <davi.p.m94@gmail.com>
2021-03-03 22:03:23 -0300

    adding pointer

--- main.go
+++ main.go
@@ -313,6 +313,24 @@ func intSeq() func() int {
 	}
 }
 
+// Recursion
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	return n * fact(n-1)
+}
+
+// Pointers
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
 // main application
 func main() {
 	helloWorld()
@@ -348,4 +366,15 @@ func main() {
 
 	newInt := intSeq()
 	fmt.Println(newInt())
+
+	fmt.Println(fact(7))
+
+	i := 1
+	fmt.Println("initial:", i)
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("zeroptr:", &i)
 }
