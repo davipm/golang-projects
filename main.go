f88e2cfefe507890820153594fa30aaf215313d9
davi <davi.p.m94@gmail.com>
2021-03-04 00:14:04 -0300

    adding methods

--- main.go
+++ main.go
@@ -343,6 +343,19 @@ func newPerson(name string) *person {
 	return &p
 }
 
+// Methods
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
+}
+
 // main application
 func main() {
 	helloWorld()
@@ -401,4 +414,12 @@ func main() {
 	fmt.Println(sp.age)
 	sp.age = 51
 	fmt.Println(sp.age)
+
+	r := rect{width: 10, height: 5}
+	fmt.Println("area", r.area())
+	fmt.Println("perim", r.perim())
+
+	rp := &r
+	fmt.Println("area", rp.area())
+	fmt.Println("perim", rp.perim())
 }
