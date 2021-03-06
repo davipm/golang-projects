35daecd18d2873961c2e652c627eabcb05bde91d
davi <davi.p.m94@gmail.com>
2021-03-06 16:33:44 -0300

    adding Channel Directions

--- goRoutineExample.go
+++ goRoutineExample.go
@@ -20,6 +20,16 @@ func worker(done chan bool) {
 	done <- true
 }
 
+// Channel Directions
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
 func main() {
 	f("direct")
 
@@ -40,4 +50,11 @@ func main() {
 	done := make(chan bool, 1)
 	go worker(done)
 	<-done
+
+	// Channel Directions
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
