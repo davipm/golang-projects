8d6ea141a52742644a1671b2ec6b524e68af8643
davi <davi.p.m94@gmail.com>
2021-03-10 19:18:16 -0300

    adding timers

--- go-by-examples/timers.go
+++ go-by-examples/timers.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	var timer1 = time.NewTimer(2 * time.Second)
+	<-timer1.C
+	fmt.Println("time 1 fired")
+
+	var timer2 = time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("time 2 fired")
+	}()
+
+	var stop2 = timer2.Stop()
+
+	if stop2 {
+		fmt.Println("Timer 2 stopped")
+	}
+
+	time.Sleep(2 * time.Second)
 }
