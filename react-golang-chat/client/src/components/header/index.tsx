1cde2ec357b31aef29975bf167048c4425fd06fa
davi <davi.p.m94@gmail.com>
2021-03-13 16:24:28 -0300

    adding header

---
+++ react-golang-chat/client/src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export default function Header() {
+  return (
+    <Container>
+      <h2>Realtime Chat App</h2>
+    </Container>
+  );
+}
---
+++ react-golang-chat/client/src/components/header/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.header`
+  background-color: #15223b;
+  color: white;
+  max-width: 100%;
+  padding: 10px;
+
+  h2 {
+    padding: 0;
+    margin: 0;
+  }
+`;
