1688553ab176a65be6b4097d50b793db8c06757c
davi <davi.p.m94@gmail.com>
2021-03-13 16:51:24 -0300

    fix typo

--- react-golang-chat/websocket/websocket.go
+++ react-golang-chat/websocket/websocket.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-var Upgrader = websocket.Upgrader{
+var Upgraded = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
@@ -15,7 +15,7 @@ var Upgrader = websocket.Upgrader{
 }
 
 func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	var conn, err = Upgrader.Upgrade(w, r, nil)
+	var conn, err = Upgraded.Upgrade(w, r, nil)
 
 	if err != nil {
 		fmt.Println(err)
