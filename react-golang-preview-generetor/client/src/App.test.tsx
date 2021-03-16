f93ac39db786e388e2e30fdb75bf7a45c711f415
davi <davi.p.m94@gmail.com>
2021-03-16 19:24:02 -0300

    update test

--- react-golang-preview-generetor/client/src/App.test.tsx
+++ react-golang-preview-generetor/client/src/App.test.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 describe("<App />", () => {
   it("should render the App", () => {
     const { container } = render(<App />);
     expect(container).toBeTruthy();
+  });
+
+  it("should test the input", () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    const inputMessage: any = getByPlaceholderText("Enter a website");
+    const submit: any = getByText("Generate!");
+
+    fireEvent.change(inputMessage, { target: { value: "Text" } });
+    expect(inputMessage.value).toBe("Text");
+
+    fireEvent.change(inputMessage, { target: { value: "" } });
+    expect(inputMessage.value).toBe("");
+
+    fireEvent.click(submit);
   });
 });
