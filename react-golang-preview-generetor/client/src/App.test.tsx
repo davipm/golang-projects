d80d20b30146a2b4845492d0869d73c6cacaa70d
davi <davi.p.m94@gmail.com>
2021-03-17 00:49:46 -0300

    update test

--- react-golang-preview-generetor/client/src/App.test.tsx
+++ react-golang-preview-generetor/client/src/App.test.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { render, fireEvent } from "@testing-library/react";
+import { QueryClientProvider, QueryClient } from "react-query";
 import App from "./App";
+
+const queryClient = new QueryClient();
+const Wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
 describe("<App />", () => {
   it("should render the App", () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <Wrapper>
+        <App />
+      </Wrapper>
+    );
     expect(container).toBeTruthy();
   });
 
   it("should test the input", () => {
-    const { getByPlaceholderText, getByText } = render(<App />);
+    const { getByPlaceholderText, getByText } = render(
+      <Wrapper>
+        <App />
+      </Wrapper>
+    );
     const inputMessage: any = getByPlaceholderText("Enter a website");
     const submit: any = getByText("Generate!");
 
