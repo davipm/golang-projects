88d966f38dca0a8997b2764451f1ced926d6b004
davi <davi.p.m94@gmail.com>
2021-03-19 13:23:56 -0300

    adding create task function

--- golang-restapi-crud/server.go
+++ golang-restapi-crud/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -27,9 +29,27 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Golang Rest api")
 }
 
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid task data")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", createTask).Methods("POST")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
