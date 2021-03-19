93ed44b42fb15c3803782694c5c0a0b3496b812b
davi <davi.p.m94@gmail.com>
2021-03-19 16:46:00 -0300

    update order

--- react-golang-preview-generetor/client/src/App.tsx
+++ react-golang-preview-generetor/client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useState, useRef } from "react";
-import axios from "axios";
 import { useMutation } from "react-query";
+import axios from "axios";
 
 interface ImagePreview {
   screenshot: string;
