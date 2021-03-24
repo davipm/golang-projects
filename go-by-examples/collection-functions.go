88c7edf332995262f8cc6fcaa0f9d63ded334013
davi <davi.p.m94@gmail.com>
2021-03-24 20:38:04 -0300

    adding collection function

---
+++ go-by-examples/collection-functions.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Index returns the first index of the target string t, or -1 if no match is found.
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
+// Include returns true if the target string t is in the slice.
+func Include(vs []string, t string) bool {
+	return Index(vs, t) >= 0
+}
+
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+}
