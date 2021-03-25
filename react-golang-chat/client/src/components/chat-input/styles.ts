3095166f891fc50f32c434b24373a53a890cbe01
davi <davi.p.m94@gmail.com>
2021-03-25 19:14:44 -0300

    update input css

--- react-golang-chat/client/src/components/chat-input/styles.ts
+++ react-golang-chat/client/src/components/chat-input/styles.ts
@@ -2,19 +2,24 @@ import styled from "styled-components/macro";
 
 export const Container = styled.div`
   display: block;
-  margin: auto;
   width: 95%;
+  margin: auto;
 
   input {
-    padding: 10px;
-    margin: 0;
-    width: 98%;
-    font-size: 16px;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    box-shadow: 0 5px 15px -5px rgba(0, 0, 0, 0.1);
+    display: block;
+    width: 100%;
+    height: 40px;
+    padding: .3rem .7rem;
+    font-size: 1.2rem;
+    font-weight: 300;
+    line-height: 1.5;
+    color: black;
+    background-color: #FFF;
+    background-clip: padding-box;
+    border: 1px solid #c3c3c3;
+    border-radius: .25rem;
     outline: 0;
-    transition: all 0.15s ease-in-out;
+    transition: all .15s ease-in-out;
 
     :focus {
       box-shadow: 0 0 6px rgba(0, 0, 0, 0.6);
